b2fc640b92d0f284c097429191167f7ed0098b0d
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-12-12 19:52:59 -0700

    Add styles to animate buttons, and worked on query for button functionality.

--- main.js
+++ main.js
@@ -23,14 +23,28 @@ let operator = "";
 const operate = function (number1, op, number2) {
   if(op == "+") {
       return add(number1, number2);
-  }else if(op == "-") {
+  } else if(op == "-") {
       return subtract(number1, number2);
-  }else if(op == "x") {
-    return multiply(number1, number2);
-  }else if(op == "÷") {
-    return divide(number1, number2);
-  }else {
-    return "Something went wrong";
+  } else if(op == "x") {
+      return multiply(number1, number2);
+  } else if(op == "÷") {
+      return divide(number1, number2);
+  } else {
+      return "Something went wrong";
   }
-
 }
+
+const btns = document.querySelectorAll('button');
+btns.forEach((button) => {
+  button.addEventListener('click', () => {
+    let evaluate = Number(button.value);
+    if(typeof(Number(button.value)) === "number") {
+      firstNumber += button.value;
+    }
+    if(button.value == ("+"||"-"||"x"||"÷")) {
+      operator = button.value;
+    };
+
+    secondNumber += button.value;
+  })
+})
